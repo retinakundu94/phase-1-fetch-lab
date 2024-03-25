12d8a9edadc1bfcad30bc108518cefeada931874
Retina Kundu <retinakundu94@gmail.com>
2024-03-25 19:16:00 -0400

    add file

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  // Return the fetch call so that it's promise can be accessed outside
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => {
+      // Check if the response is ok (status in the range 200-299)
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // Convert the response to JSON
+      return response.json();
+    })
+    .then(data => {
+      // Call renderBooks with the JSON data
+      renderBooks(data);
+    })
+    .catch(error => {
+      // Log any errors to the console
+      console.error('There has been a problem with your fetch operation:', error);
+    });
 }
 
 function renderBooks(books) {
